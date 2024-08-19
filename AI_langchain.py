fb4b895151b809aa2c6a98d015a2a4851a31c314
RicmwasData <djricmwas@gmail.com>
2024-08-19 19:18:08 +0300

    remove_api

--- AI_langchain.py
+++ AI_langchain.py
@@ -32,9 +32,9 @@ full_table_path = 'bigquery-public-data.iowa_liquor_sales.sales'
 
 
 # Initialize the Ai model 
-api_key='claude_api_key'
+#api_key='claude_api_key'
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620",
-                     api_key=api_key,
+                     api_key='api_key',
                      temperature=0)
 
 # create the template 
